927b0a9a5e596333b1d2d890788b8f6cb665cdd4
dharmtcloud <tvs.dharmendra@gmail.com>
2026-09-13 13:05:59 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+# Create a resource group
+resource "azurerm_resource_group" "Dharmendra_R1" {
+  name     = "Dharm_RG1"
+  location = "West Europe"
+}
